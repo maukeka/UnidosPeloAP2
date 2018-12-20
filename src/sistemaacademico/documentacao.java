c0ad345023bfd132098c2cbaa68b8b6853034489
@guardiaodudu <guardiao.dudu@gmail.com>
2018-12-19 23:45:47 -0200

    A vida é passageira, você é uma farsa, eu, uma piada, vamos seguindo felizes da vida!

---
+++ src/sistemaacademico/documentacao.java
@@ -0,0 +1,9 @@
+package sistemaacademico;
+
+public class documentacao {
+	/**
+	 * @author Laís Rainha, Japa top, Marcos focado, Dolphin, Mrs Monjo, GuardiaoDudu   
+	 * este é um teste!!!           
+	 */
+
+}
