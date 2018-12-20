719e314eef43b5d8840c1a1cc8b5152083d22d42
laisrtorres <laisribeirotorres@yahoo.com.br>
2018-12-20 01:43:24 -0200

    DORMI

--- src/sistemaacademico/SistemaAcademico.java
+++ src/sistemaacademico/SistemaAcademico.java
@@ -1343,7 +1343,7 @@ public class SistemaAcademico {
 		System.out.println(".__________________________________________________________________________.");
 		System.out.println("|                                                                          |");
 		System.out.println("|                                                                          |");
-		System.out.println("|       SISTEMA DE GERENCIAMENTO ACADEMICO                                 |");
+		System.out.println("|       SISTEMA DE GERENCIAMENTO ACADÊMICO                                 |");
 		System.out.println("|       Deseja adicionar alguma informação neste campo?                    |");
 		System.out.println("|        1 - Sim                                                           |");
 		System.out.println("|        2 - Não                                                           |");
@@ -1354,7 +1354,10 @@ public class SistemaAcademico {
 
 	public static boolean checkLetters(String str) //OK
 	{
-		return str.matches("[a-zA-Z\\u0020]+");
+		return str.matches("[a-zA-Z\\u0020\u00e1\u00e0\u00e2\u00e3\u00e4\u00c1\u00c0\u00c2\u00c3\u00c4"
+				+ "\u00e9\u00e8\u00ea\u00ea\u00c9\u00c8\u00ca\u00cb\u00ed\u00ec\u00ee\u00ef\u00cd\u00cc\u00ce"
+				+ "\u00cf\u00f3\u00f2\u00f4\u00f5\u00f6\u00d3\u00d2\u00d4\u00d5\u00d6\u00fa\u00f9\u00fb\u00fc"
+				+ "\u00da\u00d9\u00db\u00e7\u00c7]+");
 	}
 
 }
